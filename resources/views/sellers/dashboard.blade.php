004eaabac7b11456621f6bf6f830c5bce06cf1cc
amado <amadocahuazavargas@gmail.com>
2026-01-06 03:56:43 -0500

    u

--- resources/views/sellers/dashboard.blade.php
+++ resources/views/sellers/dashboard.blade.php
@@ -1,4 +1,6 @@
-<x-app-layout>
+@extends('layouts.app')
+
+@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             
@@ -165,4 +167,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
